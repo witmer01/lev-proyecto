52e212921c451f5a880986c0b1a27103fb2e0500
Levi Arista <levi.arista@gmail.com>
2017-07-17 17:50:46 -0500

    Taller

---
+++ resources/views/personajes.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Juego de Tronos</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  	<!-- CABECERA -->
+  	<header>
+  		    <img src="img/got.png" alt="" id="logo">
+  	</header>
+
+	<!-- CONTENEDOR PRINCIPA -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
